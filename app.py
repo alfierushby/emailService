9acf96e90864e96d7482e27c1ccceff1079b0374
ickeyben123 <rushbyalfie99@gmail.com>
2025-01-29 13:52:45 +0000

    Add health status

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import threading
 
-from flask import Flask
+from flask import Flask, jsonify
 import boto3
 from dotenv import load_dotenv
 
@@ -16,7 +16,6 @@ ACCESS_SECRET = os.getenv('AWS_SECRET_ACCESS_KEY')
 SES_SENDER_EMAIL = os.getenv("SES_SENDER_EMAIL")
 SES_RECIPIENT_EMAIL = os.getenv("SES_RECIPIENT_EMAIL")
 
-
 app = Flask(__name__)
 
 
@@ -27,9 +26,9 @@ def poll_sqs_ses_loop():
     while True:
         try:
             response = sqs_client.receive_message(
-                QueueUrl=P3_QUEUE_URL,WaitTimeSeconds=20)
+                QueueUrl=P3_QUEUE_URL, WaitTimeSeconds=20)
 
-            messages = response.get("Messages",[])
+            messages = response.get("Messages", [])
 
             if not messages:
                 print("No messages available")
@@ -46,24 +45,27 @@ def poll_sqs_ses_loop():
                               f"\nDescription: {message['description']}")
 
                 ses_client.send_email(Source=SES_SENDER_EMAIL
-                                      ,Destination={"ToAddresses": [SES_RECIPIENT_EMAIL]}
-                                      ,Message={
-                                            "Subject": {"Data": f"P3 Notification: {message['title']}"},
-                                            "Body": {"Text": {"Data": email_body}}
-                                        })
+                                      , Destination={"ToAddresses": [SES_RECIPIENT_EMAIL]}
+                                      , Message={
+                        "Subject": {"Data": f"P3 Notification: {message['title']}"},
+                        "Body": {"Text": {"Data": email_body}}
+                    })
                 print("Sent email")
-                sqs_client.delete_message(QueueUrl=P3_QUEUE_URL,ReceiptHandle=receipt_handle)
+                sqs_client.delete_message(QueueUrl=P3_QUEUE_URL, ReceiptHandle=receipt_handle)
 
         except Exception as e:
             print(f"Error, cannot poll: {e}")
 
-
+@app.route('/health',methods=["GET"])
+def health_check():
+    """ Checks health, endpoint """
+    return jsonify({"status":"healthy"}),200
 
 if __name__ == '__main__':
     sqs_client = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY
-                              ,aws_secret_access_key=ACCESS_SECRET)
+                              , aws_secret_access_key=ACCESS_SECRET)
     ses_client = boto3.client('ses', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY
-                              ,aws_secret_access_key=ACCESS_SECRET)
+                              , aws_secret_access_key=ACCESS_SECRET)
 
     sqs_thread = threading.Thread(target=poll_sqs_ses_loop, daemon=True)
     sqs_thread.start()
